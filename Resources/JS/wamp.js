04e8613e7f20fc9d0c4bebb03a8a4ae088a25863
Ray Ortgiesen <ray.ortgiesen@gmail.com>
2014-06-30 13:23:30 -0400

    Added Offers / Round Timer

--- Resources/JS/wamp.js
+++ Resources/JS/wamp.js
@@ -1,6 +1,6 @@
 function WAMP(clientType) {
 	var myShape;
-	
+	var autobahn = require('autobahn');
 	
 	self = this;
 	self.clientType = clientType;
@@ -9,168 +9,168 @@ function WAMP(clientType) {
 	self.myPlayer;
 	self.offer;
 	
-	try {
-		var autobahn = require('autobahn');
+	/*try {
+		
 	} catch (e) {
 		// when running in browser, AutobahnJS will be included without a module system.
+	}*/
+	
+		// Set up WAMP connection to router
+		var sess;
+		var connection = new autobahn.Connection({
+			url: 'ws://tpserver.local:8080/ws',
+			realm: 'tradingpit'
+		});
+
+		// Set up 'onopen' handler
+		connection.onopen = function(session) {
+			self.sess = session;
+			var currentSubscription = null;
+			self.sess.subscribe("pit.pub."+self.sess.id, self.callbacks.onCard);
+
+			//w.wampMethods.rpcCall("signin");
+
+
+			// Subscribe to a topic
+			session.subscribe('pit.pub.offers', self.callbacks.onOffer).then(
+
+			function(subscription) {
+				// console.log("subscription successfull", subscription);
+				currentSubscription = subscription;
+				console.log("subbed to offers", subscription);
+			},
+
+			function(error) {
+				//console.log("subscription failed", error);
+			}
+
+			);
+			session.subscribe('pit.pub.clock', self.callbacks.onTick).then(
+
+			function(subscription) {
+				// console.log("subscription successfull", subscription);
+				currentSubscription = subscription;
+			},
+
+			function(error) {
+				//console.log("subscription failed", error);
+			}
+
+			);
+		};
+
+		// Open connection
+		connection.open();
 	}
-	
-	// Set up WAMP connection to router
-	var sess;
-	var connection = new autobahn.Connection({
-		url: 'ws://capricorn.local:8080/ws',
-		realm: 'tradingpit'
-	});
-	
-	// Set up 'onopen' handler
-	connection.onopen = function(session) {
-		self.sess = session;
-		var currentSubscription = null;
-		
-		w.wampMethods.rpcCall("signin");
-		
 
-		// Subscribe to a topic
-		session.subscribe('pit.pub.offers', self.callbacks.onOffer).then(
+	var aiwamp = function() {
+		this.wampMethods = {
+			test: function(){
+				alert("I'm an AI!");
+			},
+			// Define an event handler
+			onCard: function(args, kwargs, details){
 
-		function(subscription) {
-			// console.log("subscription successfull", subscription);
-			currentSubscription = subscription;
-		},
+			},
+			onTick: function(args, kwargs, details) {
 
-		function(error) {
-			//console.log("subscription failed", error);
-		}
+			},
+			onOffer: function(args, kwargs, details) {
 
-		);
-		session.subscribe('pit.pub.clock', self.callbacks.onTick).then(
+			},
+			onAccept: function(args, kwargs, details) {
 
-		function(subscription) {
-			// console.log("subscription successfull", subscription);
-			currentSubscription = subscription;
-		},
-
-		function(error) {
-			//console.log("subscription failed", error);
-		}
-
-		);
-	};
-
-	// Open connection
-	connection.open();
-}
-
-var aiwamp = function() {
-	this.wampMethods = {
-		test: function(){
-			alert("I'm an AI!");
-		},
-		// Define an event handler
-		onCard: function(args, kwargs, details){
-			
-		},
-		onTick: function(args, kwargs, details) {
-			
-		},
-		onOffer: function(args, kwargs, details) {
-			
-		},
-		onAccept: function(args, kwargs, details) {
-			
-		}
-	}
-	
-	this.wamp = new WAMP(this);
-}
-
-var playerwamp = function() {
-	this.wampMethods = {
-		test: function(){
-			alert("I'm a Player!");
-		},
-		// Define an event handler
-		onCard: function(args, kwargs, details){
-			console.log("CARD", kwargs);
-			$(".moneyCounter").html("$"+kwargs.surplus);
-			$(".value").html(kwargs.reserve);
-			reserve = kwargs.reserve;
-			self.myPlayer = kwargs;
-		},
-		onTick: function(args, kwargs, details) {
-			//console.log("Tick", args, kwargs, details);
-			$("#time").html(kwargs.minutes+":"+kwargs.seconds);
-		//	console.log("tick");
-		},
-		onOffer: function(args, kwargs, details) {
-			self.currentOffers = kwargs[opponent];
-			$.get("offer_template.html", function(d){
-				Mustache.parse(d);
-				var render = Mustache.render(d,kwargs);
-				//console.log(kwargs);
-				$('.flex-offers').html(render);
-			});
-		},
-		submitOffer: function(price) {
-			self.offer.owner = self.myPlayer;
-			self.offer.price = price;
-			console.log("self.offer= ", self.offer);	
-			w.wampMethods.rpcCall("offer");	
-		},
-		accept: function(id) {
-			console.log("Accept Offer Request");
-			var offer = self.currentOffers[id%4];
-			console.log("Offer: ", offer, self.sess);
-			w.wampMethods.rpcCall("accept");	
-		},
-		rpcCall: function(call) {
-			console.log(call);
-			if (call == "signin") {
-				self.sess.call("pit.rpc.signin", [], {
-					name: "QT",
-					position: 0,
-					role: "buyer",
-					id: self.sess.id,
-					meat: "true"
-				}).then(
-				function(r) {
-					self.sess.subscribe(r.cardURI, self.callbacks.onCard);
-					myShape = r.shape;
-					$(".my-logoDiv").load( "shapes.html  #" + myShape );
-					console.log("This player: ");
-					console.log(r);
-
-					self.sess.call("pit.rpc.offerTemplate").then(function(r){
-						self.offer = r;
-					});
-				});
-			} else if (call == "offer") {
-				self.sess.call("pit.rpc.offer", [], self.offer);
-			} else if (call == "accept") {
-				self.sess.call("pit.rpc.accept", [],
-				{
-					bidder: self.myPlayer, 	//{player object}
-					offer: offer 			//{offer object} 
-				}).then(function(r) {
-					console.log("onAccept return r: ", r);
-				},
-				function(e) {
-					console.log("Error: ", e);
-				});
 			}
 		}
+
+		this.wamp = new WAMP(this);
 	}
-	
-	this.wamp = new WAMP(this);
-}
 
-if (ai) {
-	w = new aiwamp();
-	//w.wamp.prototype.test();
-} else {
-	w = new playerwamp();
-	//w.wamp.prototype.test();
-}
+	var playerwamp = function() {
+		this.wampMethods = {
+			test: function(){
+				alert("I'm a Player!");
+			},
+			// Define an event handler
+			onCard: function(args, kwargs, details){
+				console.log("CARD", kwargs);
+				$(".moneyCounter").html("$"+kwargs.surplus);
+				$(".value").html(kwargs.reserve);
+				reserve = kwargs.reserve;
+				self.myPlayer = kwargs;
+			},
+			onTick: function(args, kwargs, details) {
+				//console.log("Tick", args, kwargs, details);
+				$("#time").html(kwargs.minutes+":"+kwargs.seconds);
+			//	console.log("tick");
+			},
+			onOffer: function(args, kwargs, details) {
+				$.get("offer_template.html", function(d){
+					Mustache.parse(d);
+					var render = Mustache.render(d,kwargs);
+					$('#offers').html(render);
+				});
+			},
+			submitOffer: function(price) {
+				self.offer.owner = self.myPlayer;
+				self.offer.price = price;
+				console.log("self.offer= ", self.offer);	
+				w.wampMethods.rpcCall("offer");	
+			},
+			accept: function(id) {
+				self.acceptedOffer = self.currentOffers[id%4];
+				w.wampMethods.rpcCall("accept");	
+			},
+			rpcCall: function(call) {
+				console.log(call);
+				if (call == "signin") {
+					self.sess.call("pit.rpc.signin", [], {
+						id: self.sess.id,
+						player: {
+							role: "buyer",
+							position: 0,
+							id: self.sess.id,
+							meat: "true",
+							name: "QT"
+						}
+					}).then(
+					function(r) {
+						myShape = r.shape;
+						$(".my-logoDiv").load( "shapes.html  #" + myShape );
 
+						self.sess.call("pit.rpc.offerTemplate").then(function(r){
+							self.offer = r;
+						});
+					});
+				} else if (call == "offer") {
 
+					self.sess.call("pit.rpc.offer", [], {
+						id: self.sess.id,
+						offer: self.offer
+					});
+				} else if (call == "accept") {
+					console.log("RPC CALL self.acceptedOffer= ", self.acceptedOffer);
+					self.sess.call("pit.rpc.accept", [],
+					{
+						id: self.sess.id,
+						offer: self.acceptedOffer 			//{offer object} 
+					}).then(function(r) {
+						console.log("onAccept return r: ", r);
+					},
+					function(e) {
+						console.log("Error: ", e);
+					});
+				}
+			}
+		}
 
+		this.wamp = new WAMP(this);
+	}
+
+	if (ai) {
+		w = new aiwamp();
+		//w.wamp.prototype.test();
+	} else {
+		w = new playerwamp();
+		//w.wamp.prototype.test();
+	}
