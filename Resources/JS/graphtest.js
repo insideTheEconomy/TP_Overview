eba9dfa8a9dcfcf915eaedb5c4685955c73189e6
W. Ryan Nestor <nestor@fuzzy-math.com>
2014-08-27 16:34:34 -0500

    setting test for players

--- Resources/JS/graphtest.js
+++ Resources/JS/graphtest.js
@@ -45,9 +45,9 @@ function connect(host){
 	$("#trans").empty();
 	$("#dist").empty();
 	
-	trans = new transactionChart("#trans", 875,645);
+	//trans = new transactionChart("#trans", 875,645);
 	//dist = new distChart("#trans", 825,665);
-	//pChart = new playerChart("#trans", 825,665);
+	pChart = new playerChart("#trans", 825,665);
 
 	//pChart.draw(players);
 	connection = new autobahn.Connection({
@@ -64,15 +64,15 @@ function connect(host){
 	//	sess.subscribe("pit.pub.phase", setPhase);
 	//	sess.subscribe("pit.pub.players", printPlayers);
 	//	sess.subscribe("pit.pub.offers", printOffers);
-	/*	sess.subscribe("pit.pub.transactions", function(a,d){
+		sess.subscribe("pit.pub.round", function(a,d){
 			//console.log("TRANSACTIONS: ",d);
-			trans.draw(d.transactions);
+			pChart.draw(d.players);
 		//	dist.draw(d.distribution);
 			
-			}); */
+			}); 
 			sess.subscribe("pit.pub.transaction", function(a,d){
 				//console.log("TRANSACTIONS: ",d);
-				trans.push(d);
+			//	trans.push(d);
 				//dist.draw(d.distribution);
 				//console.log(d.distribution);
 			}); 
