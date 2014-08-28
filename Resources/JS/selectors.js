663496582127f8ec05b3b70e3571d8afee06d13d
W. Ryan Nestor <nestor@fuzzy-math.com>
2014-08-28 14:06:20 -0500

    added applescript activation to init function

--- Resources/JS/selectors.js
+++ Resources/JS/selectors.js
@@ -1,11 +1,23 @@
  
 
-
+var exec = require('child_process').exec,
+	    child;
+	
 $(function () {
 	//Happens only FIRST time program is run
 	if (!initiated) {
 		initHTML = $(document.body).html();
 		initiated = true;
+	
+
+		child = exec('osascript -e "tell application \"TP_SharedScreen\" to activate"',
+		  function (error, stdout, stderr) {
+		    console.log('stdout: ' + stdout);
+		    console.log('stderr: ' + stderr);
+		    if (error !== null) {
+		      console.log('exec error: ' + error);
+		    }
+		});
 	}	
 	
 	//Happens on EVERY restart
